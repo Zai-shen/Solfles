5984b8654fd7eacd6674b04d496f9a69aa921a4e
Triarr <battle.wow@live.com>
2022-08-26 21:33:25 +0200

    fix

--- Assets/Scripts/soundmanager.cs
+++ Assets/Scripts/soundmanager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SoundManager : MonoBehaviour
+public class soundmanager : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
 
